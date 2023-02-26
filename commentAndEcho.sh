d6c5ca66450e9d8b544e6e92ced8189114bd310e
Drumil_Akhenia <akheniad@gmail.com>
2023-02-26 18:16:45 +0530

    learning of comment and echo

---
+++ commentAndEcho.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#echo "this is our firs                    t shellscript"
+#echo this is secon                    one bro
+echo 'this is third             one         .'
+
+echo -e "\033[0;31m fail message is here"  # this is inline comment
+echo -e "\033[0;32m success message is here"
+echo -e "\033[0;33m warning message is here"
+
+echo "this
+is our 
+first line bro!!
+"
+
+# / => show that it is not end of line next line is also part of this line
+
+echo "this \
+is \
+only one \
+line bruh\n"
+
+# '' => also know as strong quote/
+echo 'my \
+name\
+is sigle quate also\
+known as strong quote
+
+'
+# if we want to use escap charecter than we need to mention e option with echo command
+echo "this is some \n new man!!"
+
+echo -e "this is some \n forgot stuff"
+
+# other escap sequance are \v  => vertical tab and \t => horizantal tab
+
+# there are many more we explore as we required
+
+
