417873a9da0c4e5abb49468d00cef8b67ae7afc0
Drumil_Akhenia <akheniad@gmail.com>
2023-02-26 18:03:42 +0530

    added basic learning

---
+++ first.sh
@@ -0,0 +1,6 @@
+echo "this is first script"
+echo "yes this is our first one"
+ls
+pwd
+cal
+
---
+++ second.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "this is our firs                    t shellscript"
+echo this is secon                    one bro
+echo 'this is third             one         .'
+
+echo -e "\033[0;31m fail message is here"
+echo -e "\033[0;32m success message is here"
+echo -e "\033[0;33m warning message is here"
